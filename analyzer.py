6a80accf4ef04c4a2aa0dfe573adf46def4d1179
sruli gutshtein <Y0527190457@GMAIL.COM>
2026-02-11 14:36:25 +0200

    add sensitive port filtering function

--- analyzer.py
+++ analyzer.py
@@ -1,7 +1,12 @@
-from checks import is_external_ip
+from checks import is_external_ip, is_sensitive_port
 
 
 def identifying_suspicions_ips(packets):
     """פונקציה שעוברת על רשימה וממיינת את הips חיצוניים לרשימה חדשה"""
     external_ips = [packet[1] for packet in packets if is_external_ip(packet[1])]
     return external_ips
+
+def identifying_suspicions_ports(packets):
+    """פונקציה שעוברת על רשימה וממיינת את הפורטים הרגישים לרשימה חדשה"""
+    sensitive_port = [packet for packet in packets if is_sensitive_port(packet[3])]
+    return sensitive_port
--- checks.py
+++ checks.py
@@ -1,7 +1,10 @@
-from config import INTERNAL_IP
+from config import INTERNAL_IP, SENSITIVE_PORTS
 
 
 def is_external_ip(ip):
        """פונקציה שבודקת אם כתובת ip הוא חיצוני"""
        return not ip.startswith(INTERNAL_IP)
 
+def is_sensitive_port(port):
+       """פונקציה שבודקת אם כתובת port הוא רגיש"""
+       return port in SENSITIVE_PORTS
